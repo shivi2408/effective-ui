e248c498cbd28773a1f3dc390e50e416c2e24216
theadityakr <theaditykr@gmail.com>
2025-01-07 00:02:48 -0500

    distrubution, readme, package

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,48 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-
-const packageJson = require('./package.json');
+import postcss from 'rollup-plugin-postcss';
+import terser from '@rollup/plugin-terser';
+import pkg from './package.json' assert { type: 'json' };
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: packageJson.main,
+      file: pkg.main,
       format: 'cjs',
       sourcemap: true,
+      exports: 'named',
     },
     {
-      file: packageJson.module,
+      file: pkg.module,
       format: 'esm',
       sourcemap: true,
+      exports: 'named',
     },
   ],
   plugins: [
     peerDepsExternal(),
-    resolve(),
+    resolve({
+      browser: true
+    }),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({ 
+      useTsconfigDeclarationDir: true,
+      exclude: ['**/*.stories.tsx', '**/*.test.tsx', '**/*.test.ts'],
+      clean: true,
+      tsconfig: './tsconfig.json'
+    }),
+    postcss({
+      extract: 'styles.css',
+      minimize: true,
+      autoModules: true,
+      inject: false, 
+    }),
+    terser(),
   ],
-  external: ['react', 'react-dom'],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ]
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
+import './Theme/theme.css';
+export type { ThemeContextProps } from "./Theme/ThemeProvider";
+export  { ThemeProvider } from "./Theme/ThemeProvider";
+
+export type { AccordionProps } from './Components/Accordion/Accordion';
+export { default as Accordion } from './Components/Accordion/Accordion';
+
+export type {AlertProps} from './Components/Alert/Alert';
+export  { default as Alert } from './Components/Alert/Alert';
+
+export type {AppBarProps} from './Components/AppBar/AppBar';
+export { default as AppBar } from './Components/AppBar/AppBar';
+
+export type {AvatarProps} from './Components/Avatar/Avatar';
+export { default as Avatar } from './Components/Avatar/Avatar';
+
+export type {BreadcrumbsProps} from './Components/Breadcrumbs/Breadcrumbs';
+export { default as Breadcrumb } from './Components/Breadcrumbs/Breadcrumbs';
+
+export type {ButtonProps} from './Components/Buttons/Button';
 export { default as Button } from './Components/Buttons/Button';
+
+export type {CalendarProps} from './Components/Calendar/Calendar';
+export { default as Calendar } from './Components/Calendar/Calendar';
+
+export type {CheckboxProps} from './Components/Checkbox/Checkbox';
+export { default as Checkbox } from './Components/Checkbox/Checkbox';
+
+export type {ChipProps} from './Components/Chip/Chip';
+export { default as Chip } from './Components/Chip/Chip';
+
+export type {DropdownProps} from './Components/Dropdown/Dropdown';
+export { default as Dropdown } from './Components/Dropdown/Dropdown';
+
+export type {InputBoxProps} from './Components/Input/Input';
+export { default as Input } from './Components/Input/Input';    
+
+export type {InputOTPProps} from './Components/InputOTP/InputOTP';
+export { default as InputOTP } from './Components/InputOTP/InputOTP';
+
+export type {LinkProps} from './Components/Link/Link';
+export { default as Link } from './Components/Link/Link'; 
+
+export type {PopoverProps} from './Components/Popover/Popover';
+export { default as Popover } from './Components/Popover/Popover';
+
+export type {RadioProps} from './Components/Radio/Radio';
+export { default as Radio } from './Components/Radio/Radio';
+
+export type {SnippetProps} from './Components/Snippet/Snippet';
+export { default as Snippet } from './Components/Snippet/Snippet';
+
+export type {SwitchProps} from './Components/Switch/Switch';
+export { default as Switch } from './Components/Switch/Switch';
+
+export type {TableProps} from './Components/Table/Table';    
+export { default as Table } from './Components/Table/Table';
