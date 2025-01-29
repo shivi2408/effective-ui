44b233592c3bc9f982bbdf038843d4ad39cb6bf8
Summer <shivangi.verma@katalystpartners.com>
2025-01-29 01:55:14 -0800

    radio fix

--- src/Components/Radio/Radio.tsx
+++ src/Components/Radio/Radio.tsx
@@ -1,26 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import classNames from 'classnames'; 
 import './Styles/radio.css'; // Assuming this is the CSS file location
 
 export interface RadioProps {
   name: string;
   options: { value: string; label?: string }[];
+  color?: 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger';
+  size?: 'sm' | 'md' | 'lg';
   selected?: string;
   onChange?: (value: string) => void;
-  orientation?: 'row' | 'column'; // New prop for orientation
+  orientation?: 'row' | 'column'; 
   className?: string;
   style?: React.CSSProperties;
-  disabled?: boolean; // New disabled prop
+  disabled?: boolean; 
+  disableOption?: string[]; 
 }
 
 const Radio: React.FC<RadioProps> = ({
   name,
   options,
+  color = 'primary',
+  size = 'md',
   selected: selectedProp,
   onChange,
-  orientation = 'column', // Default to column orientation
+  orientation = 'column',
   className,
   style,
-  disabled = false, // Default to not disabled
+  disabled = false, 
+  disableOption = [],
 }) => {
   const [selected, setSelected] = useState(selectedProp || '');
 
@@ -31,7 +38,7 @@ const Radio: React.FC<RadioProps> = ({
   }, [selectedProp]);
 
   const handleChange = (value: string) => {
-    if (!disabled) {
+    if (!disabled && !disableOption.includes(value)) {
       setSelected(value);
       if (onChange) {
         onChange(value);
@@ -39,25 +46,48 @@ const Radio: React.FC<RadioProps> = ({
     }
   };
 
+  const radioContainerClass = classNames(
+    'radio_buttons_container',
+    `radio_buttons_container--orientation-${orientation}`,
+    `radio_buttons_container--color-${color}`, 
+    `radio_buttons_container--size-${size}`, 
+    className 
+  );
+
   return (
-    <div
-      className={`radio-input ${orientation === 'row' ? 'radio-row' : 'radio-column'} ${className}`}
-      style={style}
-    >
-      {options.map((option, index) => (
-        <label key={index} className="radio-label">
-          <input
-            type="radio"
-            name={name}
-            className="radioinput"
-            value={option.value}
-            checked={selected === option.value}
-            onChange={() => handleChange(option.value)}
-            disabled={disabled} // Disable the input if disabled is true
-          />
-          {option.label && <span className="radio-label-text">{option.label}</span>}
-        </label>
-      ))}
+    <div className={radioContainerClass} style={style}>
+      {options.map((option, index) => {
+        const isOptionDisabled = disabled || disableOption.includes(option.value);
+
+        const radioButtonClass = classNames(
+          'radio_button',
+          { 'radio_button--disabled': isOptionDisabled }
+        );
+
+        const radioCustomClass = classNames(
+          'radio_button_custom',
+          { 'radio_button_custom--disabled': isOptionDisabled }
+        );
+
+        return (
+          <div className={radioButtonClass} key={index}>
+            <input
+              name={name}
+              id={`${name}-${index}`}
+              className="radio_button_input"
+              type="radio"
+              value={option.value}
+              checked={selected === option.value}
+              onChange={() => handleChange(option.value)}
+              disabled={isOptionDisabled}
+            />
+            <label htmlFor={`${name}-${index}`} className="radio_button_label">
+              <span className={radioCustomClass}></span>
+              <div className="option">{option.label || option.value}</div>
+            </label>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/stories/Radio.stories.tsx
+++ src/stories/Radio.stories.tsx
@@ -17,6 +17,16 @@ const meta = {
       control: 'object',
       description: 'Array of options for the radio group. Each option should have a value and optionally a label.',
     },
+    color: {
+      control: 'select',
+      options: ['default', 'primary', 'secondary', 'success', 'warning','danger'],
+      description: 'Color variant of the button box.',
+    },
+    size: {
+      control: 'select',
+      options: ['sm', 'md', 'lg'],
+      description: 'Size of the button box.',
+    },
     selected: {
       control: 'text',
       description: 'The currently selected value.',
@@ -33,6 +43,10 @@ const meta = {
     disabled: {
       control: 'boolean',
       description: 'Disables all the radio buttons in the group.',
+    },
+    disableOption: {
+      control: 'object',
+      description: 'Array of options to be disabled from the radio group. ',
     },
     className: {
       control: 'text',
@@ -59,7 +73,9 @@ export const Default: Story = {
       { value: 'option2', label: 'Option 2' },
       { value: 'option3', label: 'Option 3' },
     ],
-    selected: 'option1',
+    color: 'primary',
+    size: "lg",
+    selected: "option2",
     orientation: 'column', 
     disabled: false,
   },
@@ -89,25 +105,28 @@ export const Disabled: Story = {
         { value: 'option2', label: 'Option 2' },
         { value: 'option3', label: 'Option 3' },
       ],
+      color: 'default',
       selected: 'option1',
       orientation: 'column', 
       disabled: true,
     },
   };
 
-export const WithoutLabels: Story = {
-  args: {
-    name: 'no-labels',
-    options: [
-      { value: '1' },
-      { value: '2' },
-      { value: '3' },
-    ],
-    selected: '2',
-    orientation: 'column',
-    disabled: false,
-  },
-};
+  export const DisabledOption: Story = {
+    args: {
+      name: 'default-radio',
+      options: [
+        { value: 'option1', label: 'Option 1' },
+        { value: 'option2', label: 'Option 2' },
+        { value: 'option3', label: 'Option 3' },
+        { value: 'option4', label: 'Option 4' },
+      ],
+      selected: 'option1',
+      orientation: 'column', 
+      disabled: false,
+      disableOption:['option2'],
+    },
+  };
 
 export const RowFlex: Story = {
   args: {
